9562a9d9aa4c5ca8261616f99678f50c035950f2
Kevin Ng <ngkev5@gmail.com>
2020-12-10 13:03:33 -0800

    adding solution for day 10 part 1

--- day10/day10.js
+++ day10/day10.js
@@ -13,11 +13,50 @@ const getDeviceJoltage = (adaptors) => {
 
 const partOne = () => {
     const adaptors = getInput();
-    const deviceJoltage = getDeviceJoltage(adaptors);
+    let currentJoltage = 0;
+    let differences = {};
+    const pluggedIn = {};
+    let deviceJoltage = getDeviceJoltage(adaptors);
+    adaptors.push(deviceJoltage);
 
-    for(let i = 0; i < adaptors.length; i++) {
-        if(adaptors[i] 
+    while (adaptors.length > 0) {
+        for (let i = 0; i < adaptors.length; i++) {
+            const adaptor = adaptors[i]
+            const difference = adaptor - currentJoltage;
+            differences[adaptor] = difference;
+        }
+
+        let min = Number.MAX_VALUE;
+        let minKey = 0;
+        for (difference in differences) {
+            if (differences[difference] < min) {
+                min = differences[difference];
+                minKey = difference;
+            }
+        }
+
+
+        pluggedIn[minKey] = min;
+        adaptors.splice(adaptors.indexOf(Number(minKey)), 1);
+
+        currentJoltage = minKey;
+        differences = {};
     }
+
+    let oneCount = 0;
+    let threeCount = 0;
+
+    for (let i in pluggedIn) {
+        if (pluggedIn[i] == 1) {
+            oneCount++
+        }
+
+        if (pluggedIn[i] == 3) {
+            threeCount++;
+        }
+    }
+    return { oneCount, threeCount, answer: oneCount * threeCount };
+
 }
 
 console.log(partOne());
